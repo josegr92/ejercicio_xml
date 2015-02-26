0de3eaa0e3cffd2e2102b069d7c5bf1b4203664b
Jose Garcia <josegarciaroldan@gmail.com>
2015-02-26 16:16:03 +0100

    He realizado el punto 2 del  xml.py

--- xml.py
+++ xml.py
@@ -5,3 +5,10 @@ from lxml import etree
 for directorio in directorios:
 	print "Nombre: %s Telf: %s"%(directorio.find("nombre").text,directorio.find("telefono").text)
 	print directorio.find("direccion").text
+
+print "Locales de artesanía con la categoria Gijón Card"
+for directorio in directorios:
+	categorias=directorio.find("categorias")
+	for categoria in categorias:
+		if categoria.text=="Gijón Card":
+			print directorio.find("nombre").text
