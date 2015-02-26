031fa0dff23bee22652c6e1aa9dc6044f72e0b1e
Jose Garcia <josegarciaroldan@gmail.com>
2015-02-26 14:34:29 +0100

    He realizado el punto 1 del programa  xml.py

--- xml.py
+++ xml.py
@@ -1,3 +1,7 @@
 #_*_ coding:utf-8 _*_
 
 from lxml import etree
+
+for directorio in directorios:
+	print "Nombre: %s Telf: %s"%(directorio.find("nombre").text,directorio.find("telefono").text)
+	print directorio.find("direccion").text
