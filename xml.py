71758e7ff13eea6300b071be4e273da04f762739
Jose Garcia <josegarciaroldan@gmail.com>
2015-02-26 16:23:51 +0100

    He modificado el punto 2 del xml.py por que no funcionaba

--- xml.py
+++ xml.py
@@ -2,13 +2,18 @@
 
 from lxml import etree
 
+arbol=etree.parse("artesania.xml")
+directorios=arbol.getroot()
+
+
 for directorio in directorios:
 	print "Nombre: %s Telf: %s"%(directorio.find("nombre").text,directorio.find("telefono").text)
 	print directorio.find("direccion").text
 
+print "\n"
 print "Locales de artesanía con la categoria Gijón Card"
 for directorio in directorios:
 	categorias=directorio.find("categorias")
 	for categoria in categorias:
-		if categoria.text=="Gijón Card":
+		if categoria.attrib["id"]=="2741": #Como el nombre de la categoria tiene acento da error al convertirlo, entonces lo busco por el id de la categoria.
 			print directorio.find("nombre").text
